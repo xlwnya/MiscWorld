1f876906e61d87e9f3b4d8a54fc96fe5f5aa88be
xlwnya <xlwnya@ggtea.org>
2022-04-30 23:14:05 +0900

    Capture.cs

---
+++ Camera/USharp/Capture.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Xlwnya.MiscWorld.Camera.USharp
+{
+    public class Capture : UdonSharpBehaviour
+    {
+        public Texture2D targetTexture;
+        private UnityEngine.Camera _camera;
+
+        void Start()
+        {
+            _camera = GetComponent<UnityEngine.Camera>();
+        }
+
+        private void OnPostRender()
+        {
+            targetTexture.ReadPixels(_camera.pixelRect, 0, 0, false);
+            targetTexture.Apply(false);
+        }
+    }
+}
