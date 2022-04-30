b31ce130acb68e955cf9f0e537c1fb89a6e392be
xlwnya <xlwnya@ggtea.org>
2022-04-30 23:13:15 +0900

    CameraDepth, etc.

---
+++ Camera/USharp/CameraDepthNormalsActivator.cs
@@ -0,0 +1,18 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Xlwnya.MiscWorld.Camera.USharp
+{
+    public class CameraDepthNormalsActivator : UdonSharpBehaviour
+    {
+        void Start()
+        {
+            UnityEngine.Camera target = GetComponent<UnityEngine.Camera>();
+            if (target != null)
+            {
+                target.depthTextureMode = DepthTextureMode.DepthNormals;
+                target.enabled = true;
+            }
+        }
+    }
+}
---
+++ Camera/USharp/CameraMotionVectorsActivator.cs
@@ -0,0 +1,18 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace Xlwnya.MiscWorld.Camera.USharp
+{
+    public class CameraMotionVectorsActivator : UdonSharpBehaviour
+    {
+        void Start()
+        {
+            UnityEngine.Camera target = GetComponent<UnityEngine.Camera>();
+            if (target != null)
+            {
+                target.depthTextureMode = DepthTextureMode.MotionVectors;
+                target.enabled = true;
+            }
+        }
+    }
+}
